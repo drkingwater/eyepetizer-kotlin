560ad77a814f3e6c97a7228e498d691b45209752
pxq <hao15296802206@gmail.com>
2020-07-22 22:12:33 +0800

    拆分module:main

---
+++ module/main/src/main/java/me/pxq/eyepetizer/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.pxq.eyepetizer.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Description:
+ * Author : pxq
+ * Date : 2020/7/22 9:52 PM
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_main)
+    }
+
+}
