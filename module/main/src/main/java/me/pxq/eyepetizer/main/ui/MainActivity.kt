95b3b45389e364e8e50ee3a8a76ac64e4c753a43
pxq <hao15296802206@gmail.com>
2020-07-26 18:24:19 +0800

    完成底部导航栏

---
+++ module/main/src/main/java/me/pxq/eyepetizer/main/ui/MainActivity.kt
@@ -0,0 +1,54 @@
+package me.pxq.eyepetizer.main.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import me.pxq.eyepetizer.main.R
+import me.pxq.eyepetizer.main.ui.view.EyeBottomNavView
+
+/**
+ * Description:
+ * Author : pxq
+ * Date : 2020/7/22 9:52 PM
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_main)
+
+        findViewById<EyeBottomNavView>(R.id.bottom_nav_layout)?.apply {
+            itemIconTintList = null
+            candidateIcons = mutableMapOf(
+                R.id.bottom_nav_menu_item_home to ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.main_btn_home_selected
+                )!!,
+                R.id.bottom_nav_menu_item_community to ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.main_btn_community_selected
+                )!!,
+                R.id.bottom_nav_menu_item_notification to ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.main_btn_notification_selected
+                )!!,
+                R.id.bottom_nav_menu_item_mime to ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.main_btn_mine_selected
+                )!!
+            )
+            setOnNavigationItemSelectedListener {
+                //修改选中图标
+                setCheckedIcon(it.itemId)
+                when(it.itemId){
+                    //todo 切换fragment
+                }
+                true
+            }
+            //默认选择"首页"
+            selectedItemId = R.id.bottom_nav_menu_item_home
+        }
+    }
+
+
+}
